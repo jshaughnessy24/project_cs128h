5da20534765bbf700c5c93601dbb9fa64f603e61
jshaughnessy24 <jrshaughne@gmail.com>
2024-10-28 23:45:58 -0500

    added sign_in and register_account with test cases

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,22 @@ use mongodb::{
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     // Replace the placeholder with your Atlas connection string
-    let uri = "mongodb+srv://jennys4:3tA6Ui0z2MPrUnyk@cluster0.jwcji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    // let uri = "mongodb+srv://jennys4:3tA6Ui0z2MPrUnyk@cluster0.jwcji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-    // Create a new client and connect to the server
-    let client = Client::with_uri_str(uri).await?;
+    // // Create a new client and connect to the server
+    // let client = Client::with_uri_str(uri).await?;
 
-    // Get a handle on the movies collection
-    let database = client.database("sample_mflix");
-    let my_coll: Collection<Document> = database.collection("movies");
+    // // Get a handle on the movies collection
+    // let database = client.database("sample_mflix");
+    // let my_coll: Collection<Document> = database.collection("movies");
 
-    // Find a movie based on the title value
-    let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }).await?;
+    // // Find a movie based on the title value
+    // let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }).await?;
 
-    // Print the document
-    println!("Found a movie:\n{:#?}", my_movie);
+    // // Print the document
+    // println!("Found a movie:\n{:#?}", my_movie);
+
+    // project_cs128h::sign_in("".to_string(),"".to_string(),"".to_string()).await;
+    
     Ok(())
 }
